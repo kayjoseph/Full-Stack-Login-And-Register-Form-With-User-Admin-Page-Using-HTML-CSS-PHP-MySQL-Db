3b83b698313f8678ff25f5b27ef6f448409edaf8
Joseph Kay <kayjoseph049@gmail.com>
2026-08-14 09:23:40 +0300

    update on index

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+<?php
+
+session_start();
+
+$errors = [
+    'login' => $_SESSION['login_error'] ?? '',
+    'register' => $_SESSION['register_error'] ?? '',
+];
+
+$activeForm = $_SESSION['active_form'] ?? 'login';
+
+session_unset();
+
+function showError($error) {
+    return !empty($error) ? "<p class='error-message'>$error</p>" : '';
+}
+
+function isActiveForm($formName, $activeForm) {
+    return $formName === $activeForm ? 'active' : '';
+}
+
+?>
+ 
+
+
 <!DOCTYPE html>
 <html lang="eng">
     <head>
@@ -10,7 +35,8 @@
         <div class="container">
             <div class="form-box active" id="login-form">
                 <form action="login_register.php" method="post">
-                    <h2>Login</h2>     
+                    <h2>Login</h2>  
+                    <?= showError($errors['login']); ?>   
                     <input type="email" name="email" placeholder="Email" required>  
                     <input type="password" name="password" placeholder="Password" required>
                     <button type="submit" name="login">Login</button>
