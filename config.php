2a65613f60548b42edbe5116d58b8ae1bd0974f7
Joseph Kay <kayjoseph049@gmail.com>
2026-08-17 12:43:54 +0300

    update on config

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "REOPRIME!@##@!WINQUORS";
+$database = "users_db";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("connection failed: ". $conn->connect_error);
+}
+?>
